026aadbbb36fc26ef2797f4409b00d82feedf891
Yahir Ponce <142949341+YahirPonce24@users.noreply.github.com>
2024-09-05 09:54:43 -0600

    Add files via upload

---
+++ veterinaria/src/veterinaria/Veterinaria.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package veterinaria;
+
+/**
+ *
+ * @author invitado
+ */
+public class Veterinaria {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+       
+        Perro perro1 = new Perro();
+        /*perro1.nombre = "Firulais";
+        System.out.println(perro1.nombre);*/
+        
+        perro1.setNombre("Firulais Segundo");
+        System.out.println("ฅ^•ﻌ•^ฅ <-----"+perro1.getNombre());
+        
+        
+    }
+    
+}
+    
